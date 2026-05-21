c5fbc08db6720f614506065ed9537030800c5185
root <root@srv1489383.hstgr.cloud>
2026-05-21 13:51:16 +0000

    Prevent cached 404 on detail pages

--- src/app/image-sharing/[slug]/page.tsx
+++ src/app/image-sharing/[slug]/page.tsx
@@ -2,6 +2,9 @@ import { ImageDetailPage } from "@/components/image-sharing/image-detail-page";
 import { buildPostMetadata, buildTaskMetadata } from "@/lib/seo";
 import { fetchTaskPostBySlug, fetchTaskPosts } from "@/lib/task-data";
 
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 3;
 
 export async function generateStaticParams() {
